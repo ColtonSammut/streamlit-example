7c926da24cfca01000ee543926ad5aefa8dfe8b0
ColtonSammut <91942274+ColtonSammut@users.noreply.github.com>
2024-05-08 09:44:14 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,22 @@ import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
+from roboflow import Roboflow
 
+
+rf = Roboflow(api_key="tbGiR3vVrwzLF1k6atxG")
+project = rf.workspace().project("spice-and-herbs-dataset")
+model = project.version("13").model
+
+job_id, signed_url, expire_time = model.predict_video(
+    "YOUR_VIDEO.mp4",
+    fps=5,
+    prediction_type="batch-video",
+)
+
+results = model.poll_until_video_results(job_id)
+
+streamlit.video(results)
 # num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
 # num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
 
